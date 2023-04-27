7f2941a2fb599549d0ad779ef79819ce187e979d
info <jaebum7396@naver.com>
2023-04-28 00:08:19 +0900

    refactor. gateway.uri 환경변수 설정

--- src/main/java/user/Application.java
+++ src/main/java/user/Application.java
@@ -1,9 +1,12 @@
 package user;
 
-import org.springframework.boot.SpringApplication; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableDiscoveryClient
