640599f0eabe0a0e19b795e18ba38ad0aaf6916e
jaebeom <jaebum7396@naver.com>
2023-05-17 15:48:35 +0900

    fix. 상호참조 부분 수정 및 jackson 종속성 추가(데이트타임)
    Signed-off-by: jaebeom <jaebum7396@naver.com>

--- src/main/java/user/configuration/RedisConfig.java
+++ src/main/java/user/configuration/RedisConfig.java
@@ -1,6 +1,7 @@
 package user.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,7 +30,9 @@ public class RedisConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
