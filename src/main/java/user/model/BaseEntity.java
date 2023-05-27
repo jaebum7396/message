636aaf58d6f7dd063d915e2d6f40ba815435c65d
jaebeom <jaebum7396@naver.com>
2023-05-27 22:23:22 +0900

    refactor. insertDt jsonformat 추가
    Signed-off-by: jaebeom <jaebum7396@naver.com>

--- src/main/java/user/model/BaseEntity.java
+++ src/main/java/user/model/BaseEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -30,7 +31,7 @@ import lombok.experimental.SuperBuilder;
 public abstract class BaseEntity {
     @CreatedDate
     @Column(nullable = false, updatable = false, name = "INSERT_DT")
-    @JsonIgnore
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime insertDt;
 
     @CreatedBy
@@ -39,28 +40,24 @@ public abstract class BaseEntity {
     private Long insertUserCd;
 
     @LastModifiedDate
-    @JsonIgnore
     @Column(name = "UPDATE_DT")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime updateDt;
 
     @LastModifiedBy
-    @JsonIgnore
     @Column(name = "UPDATE_USER_CD")
     private Long updateUserCd;
 
-    @JsonIgnore
     @Column(name = "DELETE_YN", length = 1, columnDefinition = "CHAR(1) DEFAULT 'N'")
-    private String deleteYn = "N";
+    protected char deleteYn;
 
-    @JsonIgnore
     @Column(name = "DELETE_DT")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime deleteDt;
 
-    @JsonIgnore
     @Column(name = "DELETE_USER_CD")
     private Long deleteUserCd;
 
-    @JsonIgnore
     @Column(name = "DELETE_REMARK")
     private String deleteRemark;
 }
