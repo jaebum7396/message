f6f25ff041f082ef5e186bc05f0538247e0b9d33
jaebum <jaebum7396@naver.com>
2023-10-02 21:22:32 +0900

    fix. 소켓 에러 처리부분 수정
    Signed-off-by: jaebum <jaebum7396@naver.com>

--- src/main/java/trade/future/service/FutureService.java
+++ src/main/java/trade/future/service/FutureService.java
@@ -53,16 +53,25 @@ public class FutureService {
     }
 
     public void onFailureCallback(String streamId) {
+        long now = System.currentTimeMillis();
+        System.out.println("check"+now);
+        long currentTime = 0;
         System.out.println("[FAILURE] >>>>> " + streamId + " 예기치 못하게 스트림이 실패하였습니다. ");
-        Optional<TradingEntity> tradingEntityOpt = Optional.ofNullable(umWebSocketStreamClient.getTradingEntity(Integer.parseInt(streamId)));
+        Optional<TradingEntity> tradingEntityOpt = tradingRepository.findByStreamId(Integer.parseInt(streamId));
         System.out.println("[RECOVER] >>>>> "+tradingEntityOpt.get().toString());
         if(tradingEntityOpt.isPresent()){
             TradingEntity tradingEntity = tradingEntityOpt.get();
             streamClose(tradingEntity.getStreamId());
-            System.out.println("[RECOVER] >>>>> "+streamId +" 번 스트림을 "+autoTradeStreamOpen(tradingEntity).getStreamId() + " 번으로 복구 합니다.");
+            while( currentTime - now< 1000){
+                // currentTime  = System.currentTimeMillis();
+                System.out.println("[RECOVER] >>>>> "+streamId +" 번 스트림을 "+autoTradeStreamOpen(tradingEntity).getStreamId() + " 번으로 복구 합니다.");
+            }
         } else {
             System.out.println("[RECOVER-ERR] >>>>> "+streamId +" 번 스트림을 복구하지 못했습니다.");
-            //onFailureCallback(streamId);
+            while( currentTime - now< 1000){
+               // currentTime  = System.currentTimeMillis();
+                onFailureCallback(streamId);
+            }
         }
     }
 
