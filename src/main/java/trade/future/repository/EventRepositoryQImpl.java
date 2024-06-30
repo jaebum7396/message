5b7eff211d519a24ec4b88a44f6a4855d2458519
jaebum7396@naver.com <jaebum7396@naver.com>
2024-07-01 01:34:17 +0900

    진입 종료 시그널 변경

--- src/main/java/trade/future/repository/EventRepositoryQImpl.java
+++ src/main/java/trade/future/repository/EventRepositoryQImpl.java
@@ -29,8 +29,8 @@ public class EventRepositoryQImpl implements EventRepositoryQ {
                 .selectFrom(qKlineEvent)
                 .join(qKlineEvent.klineEntity, qKline).fetchJoin()
                 .join(qKlineEvent.tradingEntity, qTradingEntity).fetchJoin()
-                .join(qKlineEvent.klineEntity.positionEntity, qPosition).fetchJoin()
-                .join(qKlineEvent.klineEntity.technicalIndicatorReportEntity, qTechnicalIndicatorReport).fetchJoin()
+                .join(qKline.positionEntity, qPosition).fetchJoin()
+                .join(qKline.technicalIndicatorReportEntity, qTechnicalIndicatorReport).fetchJoin()
                 .where(
                         qKlineEvent.klineEntity.symbol.eq(symbol),
                         qPosition.positionStatus.eq(positionStatus)
