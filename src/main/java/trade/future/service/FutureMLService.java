ca16ce71253358fdf875fd49704fa8feb6274ba5
jaebum7396@naver.com <jaebum7396@naver.com>
2024-09-19 19:06:24 +0900

    feat. 머신러닝 기반 매매 구현

--- src/main/java/trade/future/service/FutureMLService.java
+++ src/main/java/trade/future/service/FutureMLService.java
@@ -572,13 +572,13 @@ public class FutureMLService {
                     boolean exitFlag = false;
                     if (
                         tradingEntity.getPositionSide().equals("LONG")
-                        &&(shortSignal||neutralSignal||tradingEntity.getTrend5m().equals("SHORT"))
+                        &&(shortSignal||neutralSignal||tradingEntity.getTrend4h().equals("SHORT"))
                     ){
                         exitFlag = true;
                     };
                     if (
                         tradingEntity.getPositionSide().equals("SHORT")
-                        &&(longSignal||neutralSignal||tradingEntity.getTrend5m().equals("LONG"))
+                        &&(longSignal||neutralSignal||tradingEntity.getTrend4h().equals("LONG"))
                     ){
                         exitFlag = true;
                     };
@@ -615,17 +615,19 @@ public class FutureMLService {
 
                     if (
                         longSignal
+                        &&tradingEntity.getTrend4h().equals("LONG")
                         //&&tradingEntity.getTrend1h().equals("LONG")
-                        &&tradingEntity.getTrend15m().equals("LONG")
-                        &&tradingEntity.getTrend5m().equals("LONG")
+                        //&&tradingEntity.getTrend15m().equals("LONG")
+                        //&&tradingEntity.getTrend5m().equals("LONG")
                     ) {
                         enterFlag = true;
                         positionSide = "LONG";
                     } else if (
                         shortSignal
+                        &&tradingEntity.getTrend4h().equals("SHORT")
                         //&&tradingEntity.getTrend1h().equals("SHORT")
-                        &&tradingEntity.getTrend15m().equals("SHORT")
-                        &&tradingEntity.getTrend5m().equals("SHORT")
+                        //&&tradingEntity.getTrend15m().equals("SHORT")
+                        //&&tradingEntity.getTrend5m().equals("SHORT")
                     ){
                         enterFlag = true;
                         positionSide = "SHORT";
